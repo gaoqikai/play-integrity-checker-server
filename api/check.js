cc4306dcec8034efac87a657e5232511cf47c7a9
gaoqikai <gqk9000@outlook.com>
2022-11-30 16:09:12 +0800

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,11 +1,11 @@
 import * as jose from "jose"
 
 async function decrypt(token){
-    console.log("token: "+token);
+    console.log("\ntoken: "+token);
     let decrypt_key = Buffer.from("QmeXFVm+1mDk9iSjkpqW1XEDKYqF/1Vb+d8MFpHW+ig=", "base64");
     const { plaintext, protectedHeader } = await jose.compactDecrypt(token, decrypt_key);
     var jws = new TextDecoder().decode(plaintext);
-    console.log("jws: "+jws);
+    console.log("\njws: "+jws);
 
     const verify_key = `-----BEGIN PUBLIC KEY-----
     MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEdTrgi9WFM6mZ/SH3AMb3D0nPWIM+EA2pQO9F3tgmOjmZkX5vpTh0rPWi5uPvnpvjFbeC5d0QA1GP2lBJwv8+xg==
@@ -15,20 +15,16 @@ async function decrypt(token){
     const { payload, protectedHeader2 } = await jose.compactVerify(jws, ecPublicKey);
     let result = new TextDecoder().decode(payload);
 
-    console.log("payload: "+result)
+    console.log("\npayload: "+result)
     return result;
 }
 
 module.exports = async (req, res) => {
-
     const { token = 'none' } = req.query
-
     if (token == 'none') {
         res.status(400).send({ 'error': 'No token provided' })
         return
     }
-    
-    console.log("token: " + token)
 
     decrypt(token)
         .then(data => {
