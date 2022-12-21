365db29f5caa1052f80217c1d9094c0fb7983b51
gaoqikai <gqk9000@outlook.com>
2022-12-21 11:31:04 +0800

    Update check.js

--- api/check.js
+++ api/check.js
@@ -2,15 +2,15 @@ import * as jose from "jose"
 
 async function decrypt(token){
     console.log("\ntoken: "+token);
-    dec_key_1 = "4+suAJiXSzjEJaSyX5"
-    dec_key_2 = "+cWm4mzF2L9qQgeR+OpT/zErU="
+    var dec_key_1 = "4+suAJiXSzjEJaSyX5";
+    var dec_key_2 = "+cWm4mzF2L9qQgeR+OpT/zErU=";
     let decrypt_key = Buffer.from(dec_key_1+dec_key_2, "base64");
     const { plaintext, protectedHeader } = await jose.compactDecrypt(token, decrypt_key);
     var jws = new TextDecoder().decode(plaintext);
     console.log("\njws: "+jws);
 
-    ver_key_1 = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE80huhlJ2MoqDqwPvIUrclv5cef55vRFEKrqTqXTyEesLULec4LcTgJfMrRxuIZoNQpc"
-    ver_key_2 = "+TBrbORmtwgLvLEHBfA=="
+    var ver_key_1 = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE80huhlJ2MoqDqwPvIUrclv5cef55vRFEKrqTqXTyEesLULec4LcTgJfMrRxuIZoNQpc"
+    var ver_key_2 = "+TBrbORmtwgLvLEHBfA=="
     const verify_key = "-----BEGIN PUBLIC KEY-----"+ver_key_1+ver_key_2+"-----END PUBLIC KEY-----";
     const algorithm = 'ES256'
     const ecPublicKey = await jose.importSPKI(verify_key, algorithm)
